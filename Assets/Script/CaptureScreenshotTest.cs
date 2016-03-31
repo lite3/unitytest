97ebb7cab5bca84fbd37b84d0d1dfd4c8258adc9
lite3 <lite3@qq.com>
2016-03-31 11:26:12 +0800

    add captureScreenshotTest

---
+++ Assets/Script/CaptureScreenshotTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class CaptureScreenshotTest : MonoBehaviour {
+
+    static RawImage img = null;
+    static string filepath = null;
+	// Use this for initialization
+	void Start () {
+        if (!img)
+        {
+            img = GameObject.FindObjectOfType<RawImage>();
+        }
+        filepath = Application.persistentDataPath + "/myimage.png";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void loadImage()
+    {
+        byte[] data = File.ReadAllBytes(filepath);
+        Texture2D texture = new Texture2D(1,1);
+        texture.LoadImage(data);
+        img.texture = texture;
+    }
+
+    public void captureImage()
+    {
+        Application.CaptureScreenshot(filepath);
+        print(Application.persistentDataPath);
+    }
+}
